2ec322fbea8e6fb6a2f00b811e7fae0ff68a95cc
nullsnet <nullsnet@users.noreply.github.com>
2025-03-16 19:16:51 +0900

    OTAタスクを化

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "LowVoltageSmartElectricEnergyMeter.hpp"
 #include "SmartMeterConfig.h"
 #include <ArduinoOTA.h>
+#include <ESPmDNS.h>
 #include <M5StickC.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
@@ -11,44 +12,6 @@ WiFiClient client;
 PubSubClient mqtt(MQTT_SERVER_URL, MQTT_SERVER_PORT, client);
 LowVoltageSmartElectricEnergyMeterClass echonet;
 
-void mqttLoop() {
-    if (!mqtt.loop() || !client.connected()) {
-        static int failed_counter = 0;
-        while (!client.connected()) {
-            if (mqtt.connect(MQTT_CONNECT_ID, MQTT_CONNECT_USER, NULL, "SmartMeter/Status", 0, true, "offline")) {
-                failed_counter = 0;
-            } else {
-                failed_counter++;
-            }
-            if (failed_counter > 10) {
-                esp_restart();
-            }
-            delay(1000);
-        }
-        mqtt.loop();
-    }
-}
-
-void Bp35a1StatusChangedCallback(BP35A1::SkStatus status) {
-    mqttLoop();
-    switch (status) {
-        case BP35A1::SkStatus::uninitialized:
-            mqtt.publish("SmartMeter/Status", "uninitialized", true);
-            break;
-        case BP35A1::SkStatus::connecting:
-            mqtt.publish("SmartMeter/Status", "connecting", true);
-            break;
-        case BP35A1::SkStatus::scanning:
-            mqtt.publish("SmartMeter/Status", "scanning", true);
-            break;
-        case BP35A1::SkStatus::connected:
-            mqtt.publish("SmartMeter/Status", "connected", true);
-            break;
-        default:
-            break;
-    }
-}
-
 typedef struct {
     int32_t instantaneousPower;
     float instantCurrent_R;
@@ -56,7 +19,7 @@ typedef struct {
     float cumulativeEnergyPositive;
 } SmartMeterData;
 
-bool getData(SmartMeterData *const data, uint32_t delayms = 100, uint32_t timeoutms = 3000) {
+bool getData(SmartMeterData *const data, const uint32_t delayms = 100, const uint32_t timeoutms = 3000) {
     const auto properties = std::vector<LowVoltageSmartElectricEnergyMeterClass::Property>({
         LowVoltageSmartElectricEnergyMeterClass::Property::InstantaneousPower,
         LowVoltageSmartElectricEnergyMeterClass::Property::InstantaneousCurrents,
@@ -75,7 +38,7 @@ bool getData(SmartMeterData *const data, uint32_t delayms = 100, uint32_t timeou
     return false;
 }
 
-bool initConstantData(uint32_t delayms = 100, uint32_t timeoutms = 3000) {
+bool initConstantData(const uint32_t delayms = 100, const uint32_t timeoutms = 3000) {
     const auto properties = std::vector<LowVoltageSmartElectricEnergyMeterClass::Property>({
         LowVoltageSmartElectricEnergyMeterClass::Property::Coefficient,
         LowVoltageSmartElectricEnergyMeterClass::Property::CumulativeEnergyUnit,
@@ -89,18 +52,81 @@ bool initConstantData(uint32_t delayms = 100, uint32_t timeoutms = 3000) {
     return false;
 }
 
-void WiFiEvent(WiFiEvent_t event) {
-    switch (event) {
-        case SYSTEM_EVENT_STA_GOT_IP:
-            mqtt.publish("SmartMeter/Status", "online");
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED:
-            WiFi.disconnect();
-            WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
-            delay(WIFI_DELAY);
-            break;
-        default:
-            break;
+void reconnectWiFi() {
+    log_i("connecting to %s", WIFI_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
+    while (WiFi.status() != WL_CONNECTED) {
+        log_i("waiting for connection... (%d)", WiFi.status());
+        delay(1000);
+    }
+}
+
+void otaTask(void *const param) {
+    ArduinoOTA.setTimeout(60000);
+    ArduinoOTA
+        .setHostname(HOSTNAME)
+        .setPort(3232)
+        .onStart([]() {
+            log_i("Start updating %s", ArduinoOTA.getCommand() == U_FLASH ? "sketch" : "filesystem");
+        })
+        .onEnd([]() {
+            log_i("\nEnd");
+        })
+        .onProgress([](const unsigned int progress, const unsigned int total) {
+            log_i("Progress: %u%%\r", (progress / (total / 100)));
+        })
+        .onError([](const ota_error_t error) {
+            log_i("Error[%u]: ", error);
+            switch (error) {
+                case OTA_AUTH_ERROR:
+                    log_i("Auth Failed");
+                    break;
+                case OTA_BEGIN_ERROR:
+                    log_i("Begin Failed");
+                    break;
+                case OTA_CONNECT_ERROR:
+                    log_i("Connect Failed");
+                    break;
+                case OTA_RECEIVE_ERROR:
+                    log_i("Receive Failed");
+                    break;
+                case OTA_END_ERROR:
+                    log_i("End Failed");
+                    break;
+                default:
+                    log_i("Unknown");
+                    break;
+            }
+        });
+
+    log_i("Start OTA Task");
+
+    WiFi.setHostname(HOSTNAME);
+    WiFi.onEvent([](const WiFiEvent_t event) {
+        log_i("WiFi event: %d, WiFi status : %d", event, WiFi.status());
+        switch (event) {
+            case SYSTEM_EVENT_STA_DISCONNECTED:
+                reconnectWiFi();
+                break;
+            case SYSTEM_EVENT_STA_GOT_IP:
+                log_i("WiFi connected, IP: %s", WiFi.localIP().toString().c_str());
+                MDNS.begin(HOSTNAME);
+                ArduinoOTA.begin();
+                log_i("OTA Ready");
+                while (mqtt.connected() == false) {
+                    mqtt.connect(MQTT_CONNECT_ID, MQTT_CONNECT_USER, NULL, "SmartMeter/Status", 0, true, "offline");
+                    delay(1000);
+                }
+                log_i("MQTT Ready");
+                mqtt.publish("SmartMeter/Status", "online");
+                break;
+        }
+    });
+    reconnectWiFi();
+
+    while (true) {
+        ArduinoOTA.handle();
+        delay(100);
     }
 }
 
@@ -108,11 +134,29 @@ void setup() {
     M5.begin();
     M5.Axp.ScreenBreath(0);
     M5.Lcd.setRotation(3);
-    WiFi.onEvent(WiFiEvent);
+
     mqtt.setKeepAlive(60);
     wisun.begin(115200, SERIAL_8N1, BP35A1_RX, BP35A1_TX, false, 20000UL);
-    wisun.setStatusChangeCallback(Bp35a1StatusChangedCallback);
-    WiFi.setHostname("esp32-broute2mqtt-smartmeter");
+    wisun.setStatusChangeCallback([](const BP35A1::SkStatus status) {
+        switch (status) {
+            case BP35A1::SkStatus::uninitialized:
+                mqtt.publish("SmartMeter/Status", "uninitialized", true);
+                break;
+            case BP35A1::SkStatus::connecting:
+                mqtt.publish("SmartMeter/Status", "connecting", true);
+                break;
+            case BP35A1::SkStatus::scanning:
+                mqtt.publish("SmartMeter/Status", "scanning", true);
+                break;
+            case BP35A1::SkStatus::connected:
+                mqtt.publish("SmartMeter/Status", "connected", true);
+                break;
+            default:
+                break;
+        }
+    });
+
+    xTaskCreatePinnedToCore(otaTask, "OTA_Task", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
@@ -125,16 +169,6 @@ void loop() {
         display = !display;
     }
     M5.Axp.ScreenBreath(display ? 12 : 0);
-
-    if (WiFi.status() != WL_CONNECTED) {
-        WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(WIFI_DELAY);
-        }
-        ArduinoOTA.begin();
-    }
-
-    ArduinoOTA.handle();
 
     if (wisun.getSkStatus() != BP35A1::SkStatus::connected) {
         if (wisun.initialize(50) == false) {
@@ -153,7 +187,7 @@ void loop() {
         log_i("SyntheticTransformationRatio: %d", echonet.syntheticTransformationRatio);
     }
 
-    mqttLoop();
+    mqtt.loop();
 
     SmartMeterData data = {0, 0, 0, 0};
     if (getData(&data) == false ||
